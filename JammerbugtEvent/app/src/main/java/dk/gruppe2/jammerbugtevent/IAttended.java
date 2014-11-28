39183e51827e5b6b2ba263659298cf77340273ff
Mathias Larsen <mathiasbaech@outlook.com>
2014-11-28 11:02:36 +0100

    GUI Prototype

---
+++ JammerbugtEvent/app/src/main/java/dk/gruppe2/jammerbugtevent/IAttended.java
@@ -0,0 +1,13 @@
+package dk.gruppe2.jammerbugtevent;
+
+import android.net.Uri;
+
+/**
+ * Created by Mathias on 28-11-2014.
+ */
+public class IAttended implements AttendedFragment.OnFragmentInteractionListener {
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ JammerbugtEvent/app/src/main/java/dk/gruppe2/jammerbugtevent/IMyActivities.java
@@ -0,0 +1,13 @@
+package dk.gruppe2.jammerbugtevent;
+
+import android.net.Uri;
+
+/**
+ * Created by Mathias on 28-11-2014.
+ */
+public class IMyActivities implements MyActivitiesFragment.OnFragmentInteractionListener {
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
